77b999af657577e3eb7e6b8d5b8ec0814f04dd38
Biroshop <isabelle@obirodamoda.com.br>
2026-06-02 17:19:20 -0300

    Update and rename model: 'llama3-70b-8192' to generate.js

--- api/generate.js
+++ api/generate.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${process.env.GROQ_API_KEY}`
       },
       body: JSON.stringify({
-        model: 'llama3-70b-8192',
+        model: 'mixtral-8x7b-32768',
         messages: groqMessages,
         max_tokens: max_tokens || 1000
       })
